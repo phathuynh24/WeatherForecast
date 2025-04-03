bec15df2011b90b9d1526ee836479e0489d78429
Huynh Tien Phat <2409huynhphat@gmail.com>
2025-04-03 11:21:54 +0700

    feat: add Firebase integration with cloud functions and dependencies

--- weather_forecast/macos/Flutter/GeneratedPluginRegistrant.swift
+++ weather_forecast/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import cloud_functions
 import connectivity_plus
+import firebase_auth
+import firebase_core
+import firebase_storage
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
