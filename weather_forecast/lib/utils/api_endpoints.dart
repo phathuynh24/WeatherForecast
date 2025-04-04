63b64cc597f2482183565508497886423f0b3006
Huynh Tien Phat <2409huynhphat@gmail.com>
2025-04-04 08:12:02 +0700

    refactor: remove unused API and network utility classes

--- weather_forecast/lib/utils/api_endpoints.dart
+++
@@ -1,7 +0,0 @@
-class ApiEndpoints {
-  static const String baseUrl = 'https://api.example.com';
-  static const String login = '$baseUrl/login';
-  static const String register = '$baseUrl/register';
-  static const String currentWeather = '$baseUrl/current.json';
-  static const String forecastWeather = '$baseUrl/forecast.json';
-}
