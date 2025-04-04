cae76b9bff9ce60033017bee34e47b15345c7466
Huynh Tien Phat <2409huynhphat@gmail.com>
2025-04-04 08:14:07 +0700

    feat: add weather icon URL to WeatherForecast and WeatherToday models

--- weather_forecast/lib/models/weather_forecast.dart
+++ weather_forecast/lib/models/weather_forecast.dart
@@ -3,21 +3,24 @@ class WeatherForecast {
   final double temperature;
   final double windSpeed;
   final int humidity;
+  final String weatherIconUrl;
 
   WeatherForecast({
     required this.date,
     required this.temperature,
     required this.windSpeed,
     required this.humidity,
+    required this.weatherIconUrl,
   });
 
-  // Factory method để tạo object từ JSON
+  // Factory method to create a WeatherForecast instance from JSON
   factory WeatherForecast.fromJson(Map<String, dynamic> json) {
     return WeatherForecast(
       date: json['date'],
       temperature: json['day']['avgtemp_c'],
-      windSpeed: json['day']['maxwind_kph'],
+      windSpeed: json['day']['maxwind_kph'] * 0.277778, // From wind_kph to m/s
       humidity: json['day']['avghumidity'],
+      weatherIconUrl: 'https:${json['day']['condition']['icon']}',
     );
   }
 }
--- weather_forecast/lib/models/weather_today.dart
+++ weather_forecast/lib/models/weather_today.dart
@@ -4,6 +4,7 @@ class WeatherToday {
   final double windSpeed;
   final int humidity;
   final String weatherDescription;
+  final String weatherIconUrl;
 
   WeatherToday({
     required this.cityName,
@@ -11,16 +12,18 @@ class WeatherToday {
     required this.windSpeed,
     required this.humidity,
     required this.weatherDescription,
+    required this.weatherIconUrl,
   });
 
-  // Factory method để tạo object từ JSON
+  // Factory method to create a WeatherToday instance from JSON
   factory WeatherToday.fromJson(Map<String, dynamic> json) {
     return WeatherToday(
       cityName: json['location']['name'],
       temperature: json['current']['temp_c'],
-      windSpeed: json['current']['wind_kph'],
+      windSpeed: json['current']['wind_kph'] * 0.277778, // From wind_kph to m/s
       humidity: json['current']['humidity'],
       weatherDescription: json['current']['condition']['text'],
+      weatherIconUrl: 'https:${json['current']['condition']['icon']}',
     );
   }
 }
